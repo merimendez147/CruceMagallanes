19e5a123b0bf61a2114b0b09dd6c36a27d36c381
Maria Mendez <70541977+merimendez147@users.noreply.github.com>
2021-12-13 09:34:33 -0300

    Create GestorFerry.java

---
+++ GestorFerry.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CruceMagallanes;
+
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Maria Mendez
+ */
+public class GestorFerry {
+
+    Semaphore mutexAutos; //mutex para contar la cantidad de autos que estan en el ferry
+    Semaphore ingresarFerry; //semaforo que usa el auto para subir al ferry
+    Semaphore bajarFerry; //semaforo que usa el auto para bajar del ferry
+    int cantAutosFerry; //cantidad de autos que el ferry puede transpotar
+    int autos = 0; //autos que estan en el ferry
+    Semaphore ferryLleno; //semaforo que usa el ferry para comenzar a cruzar
+    Semaphore volverVacio; //semaforo que usa el ferry para volver vacio
+
+    public GestorFerry(int CA) {
+        mutexAutos = new Semaphore(1);
+        this.cantAutosFerry = CA;
+        ingresarFerry = new Semaphore(0);
+        bajarFerry = new Semaphore(0);
+        ferryLleno = new Semaphore(0);
+        volverVacio = new Semaphore(0);
+    }
+
+    public void habilitarIngreso() {
+        ingresarFerry.release(cantAutosFerry);
+    }
+
+    public void subirAutoFerry() {
+        try {
+            ingresarFerry.acquire();
+            mutexAutos.acquire();
+            autos++;
+            mutexAutos.release();
+            bajarFerry.release();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GestorFerry.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void cruzarLleno() {
+        try {
+            bajarFerry.acquire(cantAutosFerry);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GestorFerry.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void habilitarSalida() {
+        ferryLleno.release(cantAutosFerry);
+    }
+
+    public void bajarAutoFerry() {
+        try {
+            ferryLleno.acquire();
+            mutexAutos.acquire();
+            autos--;
+            mutexAutos.release();
+            volverVacio.release();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GestorFerry.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void volverVacio() {
+        try {
+            volverVacio.acquire(cantAutosFerry);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GestorFerry.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
